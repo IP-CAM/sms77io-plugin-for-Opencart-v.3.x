cbc86e1aa724d1678017405bac5a69808a70a940
André Matthies <matthiez@gmail.com>
2021-05-31 14:16:20 +0200

    add language de-de

--- README.md
+++ README.md
@@ -1,12 +1,18 @@
 ![Sms77.io Logo](https://www.sms77.io/wp-content/uploads/2019/07/sms77-Logo-400x79.png "Sms77.io Logo")
 
-# sms77io plugin for OpenCart 3.x
+# Offical plugin for OpenCart 3.x
 
 ## Installation
 
-1. Download the latest <a href='https://github.com/sms77io/opencart/releases'>Release</a> from GitHub
-2. Login to OpenCart backend and navigate to Extensions->Installer
-3. Click "Upload" and and select the downloaded *.zip archive
-4. Head to System->Users->User Groups and edit the Administrator group
+1. Download the <a href='https://github.com/sms77io/opencart/releases/latest'>latest Release</a> from GitHub
+2. Login to OpenCart backend and navigate to `Extensions->Installer`
+3. Click "Upload" and select the downloaded *.zip archive
+4. Head to `System->Users->User Groups` and edit the Administrator group
 5. Make sure both access- and modify permissions include "extension/module/sms77_api"
-6. Go to Marketing->Sms77.io API->Settings and enter your API key.
+6. Go to `Marketing->Sms77.io API->Settings` and enter your API key.
+
+### Support
+
+Need help? Feel free to [contact us](https://www.sms77.io/en/company/contact/).
+
+[![MIT](https://img.shields.io/badge/License-MIT-teal.svg)](./LICENSE)
---
+++ sms77_api.ocmod/upload/admin/language/de-de/extension/module/sms77_api.php
@@ -0,0 +1,53 @@
+<?php
+$_['caption_msg_history'] = 'Historie ausgehender Nachrichten';
+
+$_['error_key'] = 'Ungültiger API-Schlüssel!';
+$_['error_debug'] = 'Ungültiger Wert für "Debug"!';
+$_['error_delay'] = 'Ungültiger Wert für "Delay"!';
+$_['error_flash'] = 'Ungültiger Wert für "Flash"!';
+$_['error_foreign_id'] = 'Ungültiger Wert für Foreign ID!';
+$_['error_from'] = 'Ungültiger Wert für "Absender"';
+$_['error_label'] = 'Ungültiger Wert für "Label"!';
+$_['error_no_reload'] = 'Ungültiger Wert für "No Reload"!';
+$_['error_permission'] = 'Fehler: Mangelnde Berechtigung zum Ändern vom Modul sms77io!';
+$_['error_performance_tracking'] = 'Ungültiger Wert für "Performance Tracking"!';
+$_['error_to'] = 'Ungültiger Wert für "To"!';
+$_['error_text'] = 'Ungültiger Wert für "Text"!';
+$_['error_udh'] = 'Ungültiger Wert für "User Data Header"!';
+$_['error_unchanged'] = 'Keine Änderungen erkannt!';
+
+$_['filter_customerGroup'] = 'Kundengruppen-Filter (ignoriert wenn "Empfänger" gesetzt)';
+
+$_['heading_settings'] = 'Einstellungen - sms77io';
+$_['heading_smses'] = 'SMS-Nachrichten - sms77io';
+$_['heading_voices'] = 'Sprachnachrichten - sms77io';
+$_['heading_title'] = 'Sms77.io API';
+
+$_['placeholder_text'] = 'Hi {{firstname}} {{lastname}} ! Ist Ihre Email {{email}} korrekt?'
+ . ' Bite vergewissern Sie sich dbzgl., da wir Sie zukünftig darüber kontaktieren werden.';
+
+$_['setting_key'] = 'API-Schlüssel';
+$_['setting_debug'] = 'Debug';
+$_['setting_delay'] = 'Delay';
+$_['setting_flash'] = 'Flash';
+$_['setting_foreign_id'] = 'Foreign ID';
+$_['setting_from'] = 'Absender';
+$_['setting_label'] = 'Label';
+$_['setting_no_reload'] = 'No Reload';
+$_['setting_performance_tracking'] = 'Performance Tracking';
+$_['setting_to'] = 'Empfänger';
+$_['setting_text'] = 'Nachrichteninhalt';
+$_['setting_udh'] = 'User Data Header';
+
+$_['text_created'] = 'Erstellt';
+$_['text_edit_settings'] = 'Einstellungen ändern - sms77io';
+$_['text_extension'] = 'Erweiterungen';
+$_['text_config'] = 'Konfiguration';
+$_['text_response'] = 'Antwort';
+$_['text_msg_history'] = 'Nachrichtenverlauf';
+$_['text_send_msg'] = 'Nachricht senden';
+$_['text_write_msg'] = 'Nachricht schreiben';
+$_['text_settings_default'] = 'Standardeinstellungen';
+$_['text_settings_required'] = 'Benötigte Einstellungen';
+$_['text_settings_updated'] = 'Einstellungen für das Modul sms77io geändert!';
+$_['text_sms_sent'] = 'Der SMS-Versand via sms77.io wurde erfolgreich initiiert!';
